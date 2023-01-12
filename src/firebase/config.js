593821b287cc99fc287ab8d73f07882cd1b413a7
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-01-12 17:40:31 -0300

    miniblog em andamento

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_VfBp__w2XRYuYXCMMDpJ0FG9ay1xl6w",
+  authDomain: "miniblog-c0122.firebaseapp.com",
+  projectId: "miniblog-c0122",
+  storageBucket: "miniblog-c0122.appspot.com",
+  messagingSenderId: "129151468946",
+  appId: "1:129151468946:web:c594375b99fa88d97b2d4b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
---
+++ src/pages/About/About.js
@@ -0,0 +1,17 @@
+//CSS
+import styles from './About.module.css'
+
+
+
+
+
+const About = () => {
+
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
+}
+
+export default About;
