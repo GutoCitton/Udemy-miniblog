5c951d6c1ab29be66f9a0b041518cb8623486018
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-01-13 15:49:06 -0300

    aula_123

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <NavLink to='/' className={styles.brand}>
-        Mini <span>Blog</span>
-      </NavLink>
+        <NavLink to='/' className={styles.brand}>
+          Mini <span>Blog</span>
+        </NavLink>
       <ul className={styles.links_list}>
         <li>
           <NavLink to='/' className={({isActive}) => (isActive ? styles.active : '')}>
@@ -17,8 +17,18 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
-        <NavLink to='/about' className={({isActive}) => (isActive ? styles.active : '')}>
-            Sobre
+          <NavLink to='/login' className={({isActive}) => (isActive ? styles.active : '')}>
+              Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/register' className={({isActive}) => (isActive ? styles.active : '')}>
+              Cadastrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/about' className={({isActive}) => (isActive ? styles.active : '')}>
+              Sobre
           </NavLink>
         </li>
       </ul>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,9 +2,6 @@
 import styles from './About.module.css'
 
 
-
-
-
 const About = () => {
 
   return (
