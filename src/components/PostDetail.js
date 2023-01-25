3c5c6ba209e0e401df48a04822b1120b4d163e4c
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-01-25 15:39:00 -0300

    aula_144_finalizada

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,13 +1,13 @@
-import styles from './PostDetail.module.css'
+import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom';
+import styles from "./PostDetail.module.css";
 
-const PostDetail = ({post}) => {
+const PostDetail = ({ post }) => {
   return (
     <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <p className={styles.createdBy}>{post.createdBy}</p>
+      <p className={styles.createdby}>por: {post.createdBy}</p>
       <div className={styles.tags}>
         {post.tags.map((tag) => (
           <p key={tag}>
@@ -16,7 +16,9 @@ const PostDetail = ({post}) => {
           </p>
         ))}
       </div>
-      <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ler</Link>
+      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+        Ler
+      </Link>
     </div>
   );
 };
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,37 +1,35 @@
-
+import styles from "./Search.module.css";
 
 // hooks
-import { useFetchDocuments } from "../../hooks/useFetchDocuments"
-import { useQuery } from "../../hooks/useQuery"
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useQuery } from "../../hooks/useQuery";
 
 // components
 import PostDetail from "../../components/PostDetail";
 import { Link } from "react-router-dom";
 
-
-
 const Search = () => {
   const query = useQuery();
-  const search = query.get('q');
+  const search = query.get("q");
 
-  const {documents: posts} = useFetchDocuments('posts', search)
-  
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
-    <div>
-      <h2>Search</h2>
-      <div>
+    <div className={styles.search_container}>
+      <h1>Resultados encontrados para: {search}</h1>
+      <div className="post-list">
         {posts && posts.length === 0 && (
-          <>
+          <div className="styles.noposts">
             <p>Não foram encontrados posts a partir da sua busca...</p>
-            <Link to='/' className='btn btn-dark'>
+            <Link to="/" className="btn btn-dark">
               Voltar
             </Link>
-          </>
+          </div>
         )}
-        {posts && posts.map((post) => <PostDetail key={post.id} post={post} /> )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Search;
