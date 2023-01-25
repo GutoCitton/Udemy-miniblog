b6c482c0d191622ec52b73b5cd3173b54c962e90
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-01-25 16:17:06 -0300

    aula_147

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -8,13 +8,24 @@ import { useFetchDocument } from '../../hooks/useFetchDocument';
 
 const Post = () => {
 const { id } = useParams();
-const {document: post} = useFetchDocument('posts', id)
+const {document: post, loading} = useFetchDocument('posts', id)
 
   return (
-    <div>
+    <div className={styles.post_container}>
       {post && (
         <>
           <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Este pos trata sobre:</h3>
+          <div className={styles.tags}>
+            {post.tags.map((tag) => (
+              <p key={tag}>
+                <span>#</span>
+                {tag}
+            </p>
+            ))}
+          </div>
         </>
       )}
     </div>
