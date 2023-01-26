d8019d18c167e5c04755d9bca9785395edf5e477
GutoCitton <87103436+GutoCitton@users.noreply.github.com>
2023-01-26 15:47:41 -0300

    aula_151

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -22,7 +22,7 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -34,28 +34,30 @@ const Dashboard = () => {
         </div>
       ) : (
         <>
-          <div>
+          <div className={styles.post_header}>
             <span>Título</span>
             <span>Ações</span>
           </div>
 
-          {posts && posts.map((post) => <div>
-            <p>{post.title}</p>
-            <div>
-              <Link to={`/posts/$${post.id}`} className='btn btn-outline'>
-                Ver
-              </Link>
-              <Link to={`/posts/edit/$${post.id}`} className='btn btn-outline'>
-                Editar
-              </Link>
-              <button 
-                onClick={() => deleteDocument(post.id)}
-                className='btn btn-outline btn-danger'
-              >
-              Excluir
-              </button>
-            </div>
-          </div>)}</>
+          {posts && 
+          posts.map((post) => (
+            <div key={post.id} className={styles.post_row}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className='btn btn-outline'>
+                  Ver
+                </Link>
+                <Link to={`/posts/edit/$${post.id}`} className='btn btn-outline'>
+                  Editar
+                </Link>
+                <button 
+                  onClick={() => deleteDocument(post.id)}
+                  className='btn btn-outline btn-danger'
+                >
+                Excluir
+                </button>
+              </div>
+            </div>))}</>
       )}
 
       
